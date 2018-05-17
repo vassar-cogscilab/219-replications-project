17019fc69694aabed4f6958c3bc8fa45b9e5ee50
Caroline Beech <cbeech@vassar.edu>
2018-05-17 18:31:03 -0400

    Updated Chun file
    Excluded two subjects and reaction times over 4000
    Added some comments
    Changed effect size calculations and comments
    Now matches results in slides!

--- data-summary-chun.R
+++ data-summary-chun.R
@@ -1,23 +1,31 @@
 library(tidyverse)
 library(readr)
 
-#Need to discuss differences between these results and presentation results
-
 #Read in data
 chun.data <- read_csv('data/chun_data.csv')
 #Extract data from test phase, filtering out incorrect trial responses
 test.data <- chun.data %>% filter(phase=='test') %>% filter(correct==1)
 
+#Exclude subjects 'jwne38k3p6nbxh8' and 'rfe3y2ty3vvg1w5'
+test.data <- test.data %>% filter((subject_id != 'jwne38k3p6nbxh8') & 
+                                    (subject_id != 'rfe3y2ty3vvg1w5'))
+#Exclude reaction time outliers (over 4000)
+test.data <- test.data %>% filter(rt <= 4000)
+
 #Convert block from <chr> to <dbl> and add the variable epoch to test.data
 test.data$block <- as.numeric(test.data$block)
 blockToEpochFunction <- function(x){
   sapply(x, function(x){ #Needed because block is a vector
-  if(x >= 0 && x <= 4) 1
-  else if(x >= 5 && x <= 9) 2
-  else if(x >= 10 && x <= 14) 3
-  else NULL
+    #Blocks 0-4 correspond to Epoch 1
+    if(x >= 0 && x <= 4) 1
+    #Blocks 5-9 correspond to Epoch 2
+    else if(x >= 5 && x <= 9) 2
+    #Blocks 10-14 correspond to Epoch 3
+    else if(x >= 10 && x <= 14) 3
+    else NULL
   })}
 test.data <- mutate(test.data, epoch = blockToEpochFunction(block))
+test.data
 
 #Create a tibble called test.means with the mean reaction time for each 
  #participant in each condition
@@ -35,27 +43,16 @@ test.aov <- with(test.means,
 )
 summary(test.aov)
 
-#Compute effect size by computing the standardized difference between 
- #the cueing effect for epochs 1 and 3
+#Compute effect size by computing the standardized mean difference in
+ #reaction time for new and old stimuli in Epoch 3 (cueing effect)
 
-epoch1.cueing.effect <-
-  mean(filter(test.data, oldORnew == "new", epoch==1)$rt) -
-  mean(filter(test.data, oldORnew == "old", epoch==1)$rt)
+#Store the reaction times for new and old stimuli in Epoch 3
+epoch3.new <- filter(test.means, oldORnew == "new", epoch==3)$reaction_time
+epoch3.old <- filter(test.means, oldORnew == "old", epoch==3)$reaction_time
 
-epoch3.cueing.effect <-
-  mean(filter(test.data, oldORnew == "new", epoch==3)$rt) -
-  mean(filter(test.data, oldORnew == "old", epoch==3)$rt)
+#Compute the pairwise differences
+epoch3.diff <- epoch3.new - epoch3.old
 
-#Note: If aggregating by subject_id first, the code is slightly different:
-
-#epoch1.cueing.effect <-
-  #mean(filter(test.means, oldORnew == "new", epoch==1)$reaction_time) -
-  #mean(filter(test.means, oldORnew == "old", epoch==1)$reaction_time)
-#epoch3.cueing.effect <-
-  #mean(filter(test.means, oldORnew == "new", epoch==3)$reaction_time) -
-  #mean(filter(test.means, oldORnew == "old", epoch==3)$reaction_time)
-
-diff <- epoch3.cueing.effect - epoch1.cueing.effect
-MSE.interaction <- 22272
-d <- diff/sqrt(MSE.interaction)
+#Effect size = mean difference / sd(pairwise differences)
+d <- mean(epoch3.diff)/sd(epoch3.diff)
 d
